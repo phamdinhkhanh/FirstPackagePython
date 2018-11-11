f9c050259c73c5ae99280d7f34f3d3eebb51eebc
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2018-11-12 00:04:58 +0700

    First Python Package

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 11 23:10:57 2018
+
+@author: laptopTCC
+"""
+
+from mul import mul
+from sum import sum
---
+++ sum.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 11 23:08:32 2018
+
+@author: laptopTCC
+"""
+
+def sum(x, y):
+    print('Sum of {}, {}: {}'.format(x, y, x+y))
+    return x+y
