d9ddbbaff02c3aee4a7bc13de581589f34f820ab
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2018-11-30 12:11:27 +0700

    Add setup extension

--- src/__init__.py
+++ src/__init__.py
@@ -5,5 +5,3 @@ Created on Sun Nov 11 23:10:57 2018
 @author: laptopTCC
 """
 
-from mul import mul
-from sum import sum
---
+++ test.py
@@ -0,0 +1,4 @@
+from FirstPackagePython import mul, sum
+
+print(mul(2,3))
+print(sum(5,3))
